ad570ad763ec2bcca1c145ae712bf4b92c0267f5
Cokorda Gede Satria Widnyana Putra <coksatria2005@gmail.com>
2026-06-30 13:30:01 +0700

    feat: Perubahan fitur login dan registrasi

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,8 +1,30 @@
+﻿from typing import Optional
+
 from pydantic import BaseModel
 
 class Token(BaseModel):
     access_token: str
-    token_type: str 
+    token_type: str
 
 class TokenData(BaseModel):
     id: str
+
+class RegisterRequest(BaseModel):
+    email: str
+    password: str
+    fullname: str
+    phone: Optional[str] = None
+    position: str = "Dokter Gigi"
+
+class UserResponse(BaseModel):
+    id: str
+    email: str
+    fullname: str
+    phone: Optional[str] = None
+    position: Optional[str] = None
+    role: str
+
+class RegisterResponse(BaseModel):
+    user: UserResponse
+    access_token: str
+    token_type: str
